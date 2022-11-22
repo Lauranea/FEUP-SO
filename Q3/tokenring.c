232303117f0b08742fb2481207f7d7a99f6d58ba
AndreSantos03 <andrebernardosantos@gmail.com>
2022-11-22 20:17:49 +0000

    Some Changus

--- Q3/tokenring.c
+++ Q3/tokenring.c
@@ -10,24 +10,36 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+
+
 int main(int argc, char* argv[])
 {
     int i = 0;
-    while(i < argv[1])
-    {
-        int id = fork();
-        if(id != 0);
-        else
-        {
-            execlp();
-        }
-        i++;
+    int fd;
+    int passed = 0;
+    while(i < atoi(argv[1]))
+    { 
+        //a minha sanidade mental foi posta em questão a fazer estas duas linhas de código
+        char pipename[1024];
+        sprintf(pipename,"pipe%ito%i", i, i+1);
+
+        
+        mkfifo(pipename, 0666) //so porque apareceu assim no tutorial
+
+
+        // int id = fork();
+        // if(id != 0);
+        // else
+        // {
+        //     execlp();
+        // }
+        // i++;
     }
-    i = 0;
-    while(i < argv[1])
-    {
-        wait();
-        i++;
-    }
+    // i = 0;
+    // while(i < argv[1])
+    // {
+    //     wait();
+    //     i++;
+    // }
     return 0;
 }
