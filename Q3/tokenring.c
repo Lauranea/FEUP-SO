2d2c800d20aff2a6608528d4bdeee6e4e151a31a
AndreSantos03 <andrebernardosantos@gmail.com>
2022-11-23 11:53:19 +0000

    Update q3
    semi a funcionar?

--- Q3/tokenring.c
+++ Q3/tokenring.c
@@ -7,39 +7,45 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define BUFFER_SIZE 1024
 
 
 int main(int argc, char* argv[])
 {
-    int i = 0;
     int fd;
     int passed = 0;
-    while(i < atoi(argv[1]))
+    int i = 1;
+
+    while(i <= atoi(argv[1]))
     { 
-        //a minha sanidade mental foi posta em questão a fazer estas duas linhas de código
-        char pipename[1024];
-        sprintf(pipename,"pipe%ito%i", i, i+1);
+        char pipename[BUFFER_SIZE];
 
-        
-        mkfifo(pipename, 0666) //so porque apareceu assim no tutorial
+        if(i == atoi(argv[i])){
+            sprintf(pipename,"pipe%ito%i", i, 1);
+            i = 1;
+        }
+        else {
+            sprintf(pipename,"pipe%ito%i", i, i+1);
+            i++;
+        }
 
 
-        // int id = fork();
-        // if(id != 0);
-        // else
-        // {
-        //     execlp();
-        // }
-        // i++;
+        char passed_str[BUFFER_SIZE];
+        sprintf(passed_str,"%i",passed);
+
+        mkfifo(pipename, 0666); //criar named pipe
+
+        fd = open(pipename, O_WRONLY); //write only
+
+        write(fd,passed_str,strlen(passed_str));
+        close(fd);
+
+        passed++;
     }
-    // i = 0;
-    // while(i < argv[1])
-    // {
-    //     wait();
-    //     i++;
-    // }
+
     return 0;
 }
